a86055dfdfb5085175bf1f22d8eb442e7c8b6e80
Oliver Frost <Oliver Frost>
2016-07-20 11:37:37 +0300

    Adds fourth example that shows how to apply fixtures.

---
+++ SeleniumPresentation/Lesson2-PageObject+TestNG/ApplyFixtures/src/LoginTestWithFixtures.java
@@ -0,0 +1,41 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LoginTestWithFixtures {
+	public WebDriver driver;
+
+	@BeforeClass
+	public void startUp() {
+		driver = new FirefoxDriver();		
+	}
+
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+
+	@Test
+	public void verifyLoginWithCorrectCredentials() {
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.id("email")).sendKeys("kholodniy.cpd@gmail.com");
+		driver.findElement(By.id("pass")).sendKeys("Partyword1");
+		driver.findElement(By.id("u_0_o")).click();
+
+		Assert.assertTrue(driver.findElement(By.id("contentCol")).isDisplayed(), "[ERROR] Feed not found.");
+	}
+
+	@Test
+	public void verifyLoginWithIncorrectCredentials() {
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.id("email")).sendKeys("kholodniy.cpd@gmail.com");
+		driver.findElement(By.id("pass")).sendKeys("RandomPassword");
+		driver.findElement(By.id("u_0_o")).click();
+
+		Assert.assertFalse(driver.findElement(By.id("contentCol")).isDisplayed(), "[ERROR] Feed not found.");
+	}
+}
