449e87a8e1d7b53bdae3194a91d06bf167995a0f
Oliver Frost <Oliver Frost>
2016-07-30 09:49:24 +0300

    Adds first two examples for lesson 3

---
+++ SeleniumPresentation/Lesson2-PageObject+TestNG/BasicArchitecture/src/help1/LoginPage.java
@@ -0,0 +1,35 @@
+package help1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import framework.BasePage;
+
+public class LoginPage extends BasePage {
+
+	public LoginPage(WebDriver driver) {
+		super(driver);
+	}
+
+	public void login(String login, String password) {
+		enterLogin(login);
+		enterPassword(password);
+		clickSubmitButton();
+	}
+	
+	public void open() {
+		driver.get("https://www.facebook.com/");
+	}
+
+	public void enterLogin(String login) {
+		type(By.id("email"), login);
+	}
+
+	public void enterPassword(String password) {
+		type(By.id("pass"), password);
+	}
+
+	public void clickSubmitButton() {
+		driver.findElement(By.id("u_0_o")).click();
+	}
+}
